a96598e6373df4261a2c5c53350dd9085b7317ed
phyleria <atienophyllis.com>
2024-07-29 01:35:57 +0300

    navbar

--- src/components/Musings.js
+++ src/components/Musings.js
@@ -16,12 +16,12 @@ const Musings = () => {
     <div className="Musings">
       <div className="max-w-5xl mx-auto p-5 bg-[#f6f4ef] mt-5 mb-20 rounded-lg shadow-lg">
         <header className="flex flex-wrap justify-between items-center mb-10 p-5">
-          <div className="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-10 w-full md:w-auto">
+          <div className="flex flex-col md:flex-row items-start md:items-center space-y-4 md:space-y-0 md:space-x-10 w-full md:w-auto">
             <a href="/" className="text-2xl font-bold mr-0 md:mr-20">
               Me
             </a>
             <nav>
-              <ul className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-14 text-lg w-full md:w-auto">
+              <ul className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-14 text-lg w-full md:w-auto md:ml-10">
                 <li>
                   <a href="/devrel" className="hover:text-gray-600">
                     Learnings from DevRel
@@ -40,11 +40,11 @@ const Musings = () => {
               </ul>
             </nav>
           </div>
-
-          <button className="ml-6 mt-4 md:mt-0 px-4 py-2 border border-black hover:bg-gray-100 rounded-full">
+          <button className="mt-4 md:mt-0 px-4 py-2 border border-black hover:bg-gray-100 rounded-full">
             See my work
           </button>
         </header>
+
         <section className="p-6 rounded-lg shadow-md mb-10">
           <p className="text-lg mt-4 text-gray-600">
             This blog is my creative outlet, where I share my passions and
